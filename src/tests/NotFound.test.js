8721cbf5de57fa0bccd43347a33d44ebfc1e1bd2
Igormcf <igormaciel82@gmail.com>
2022-03-22 18:04:33 -0300

    Ends requirement 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import { NotFound } from '../components';
+
+describe('Testa o Component NotFound', () => {
+  it('Testa se a página tem uma tag h2 com Page requested not found 😭', () => {
+    render(<NotFound />);
+
+    const notFoundText = screen.getByRole('heading',
+      { name: /Page requested not found/i, level: 2 });
+    expect(notFoundText).toBeInTheDocument();
+  });
+  it('Testa se a página mostra uma imagem', () => {
+    render(<NotFound />);
+    const image = screen.getByAltText(/Pikachu crying because/i);
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
